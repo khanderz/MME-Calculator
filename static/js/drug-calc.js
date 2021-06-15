79ae76219a3ea1e408a8175f89ffc7fa02a05b91
Khanh Mai <khanhmai@creighton.edu>
2021-06-15 20:51:28 +0000

    restructured js file

--- static/js/drug-calc.js
+++ static/js/drug-calc.js
@@ -1,97 +1,120 @@
-console.log('js is working')
+"use strict";
+
+console.log('js is working');
 
 // create a reset button that will reset MMECalcTotal back to 0
-let MMECalcTotal = 0
+let MMECalcTotal = 0;
+
+// Recalculate/update total MME based on meds in #med-list
+const updateTotalMME = () => {
+    let MMETotal = 0;
+
+    // Get all .med-list-mme <td> elements
+    $('.med-list-mme').each((idx, el) => {
+        console.log(el);
+
+        MMETotal += Number($(el).html());
+        
+        console.log(`Total after loop # ${idx}: ${MMETotal}`);
+    });
+
+    $('#mme-total').html(MMETotal);
+};
+
+// Add medication to medlist table
+// Args:
+// medData (object) - an object that looks like:
+//   {
+//     drug: drug name,
+//     dose: drug dose,
+//     quantity: quantity of drug,
+//     days_supply: days supply
+//  }
+const addMedToMedlist = (medData) => {
+    // Create delete button
+    const button = $(document.createElement('button'));
+    button.html('Delete drug');
+
+    // Add event handler to delete button that deletes the button's row
+    $(button).on('click', (evt) => {
+        // Get the button's row and delete it
+        const rowToDelete = $(button).parent();
+        rowToDelete.remove();
+
+        updateTotalMME();
+    });
+    
+    // Build tr element
+    const tr = $(document.createElement('tr'));
+    tr.attr('class', 'medication');
+
+    // Append data from medData to row
+    tr.append(`
+        <td class="med-list-drug">${medData.drug}</td>
+        <td>${medData.dose}</td>
+        <td>${medData.quantity}</td>
+        <td>${medData.days_supply}</td>
+    `);
+    
+    // Calculate MME for medData and add to #med-list table
+    $.get('/results', medData, (data) => {
+        tr.append(`<td class="med-list-mme">${data.MME}</td>`);
+        tr.append(button);
+        
+        // Append tr to #med-list
+        $('#med-list').append(tr);
+        updateTotalMME();
+    });
+};
 
 // takes user inputs and assigns them to "params"
 const handleCalculate = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+
     const params = {
-        'drug': document.querySelector('#drug').value,
+        'drug': $('#drug').val(),
         'dose': document.querySelector('#dose').value,
         'quantity': document.querySelector('#quantity').value, 
         'days_supply': document.querySelector('#days_supply').value
-    }
+    };
 
-    console.log(params)
-
-    // increment MME 
-    const incrementMME = (MME) => {
-        const MMEtotal = $('#mme-total')
-
-        MMECalcTotal += MME
-        
-        console.log(MMECalcTotal)
-
-        MMEtotal.html(MMECalcTotal);
-        console.log(MME, '******* Increment MME *******')
-    }
-
-    // populates total MME
-    $.get('/results', params, (data) => {
-        console.log(data, '*********DATA*******');
-        $('#mme-total').html(data.MME);
-        MME = data.MME;
-        incrementMME(MME);
-        $('#med-list').append(`<tr> <td> ${data.MME} </td> </tr>`);
-    })
-
-    // appends med list
-    $('#med-list').append(`
-    <tr>
-      <td> 
-      ${document.querySelector('#drug').value} 
-      ${document.querySelector('#dose').value} 
-      ${document.querySelector('#quantity').value} 
-      ${document.querySelector('#days_supply').value}
-      </td>
-    </tr>`);
-
-    // create delete button
-    const button = document.createElement("BUTTON");
-        button.innerText = 'Delete drug';
+    console.log(params);
     
-    $('#med-list').append(`
-    <tr>
-      <td> 
-      ${button}
-      </td>
-    </tr>`);
+    addMedToMedlist(params);
 };
 
 // takes in user inputs and assigns it "formData"
 const handleAddMed = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+
     const formData = {
         'drug': document.querySelector('#drug').value,
         'dose': document.querySelector('#dose').value,
         'quantity': document.querySelector('#quantity').value, 
         'days_supply': document.querySelector('#days_supply').value    
-    }
-    console.log(formData)
+    };
+
+    console.log(formData);
 
     // adds to user.med_list in db
     $.post('/add', formData, (response) => {
-        console.log(response)
-    })
-}
+        console.log(response);
+        if (response.msg === 'medication added') {
+            addMedToMedlist(formData);
+        }
+    });
+};
 
 // Reset med list button
 const clearMedList = () => {
-    $('#mme-total').html('0');
     $('#med-list').empty();
-    MMECalcTotal = 0;
-    console.log(MMECalcTotal, '******* MME total reset *******');
+    updateTotalMME();
 };
 
 // Delete a drug from the med list
-const deleteDrug =
+// const deleteDrug =
 
 // event listeners
-document.getElementById('drug-form').addEventListener('submit', handleCalculate)
-document.getElementById('add-med-button').addEventListener('click', handleAddMed)
-document.getElementById('clear-med-list').addEventListener('click', clearMedList)
-
-
-// REACT
-// ReactDOM.render();
+document.getElementById('drug-form').addEventListener('submit', handleCalculate);
+document.getElementById('add-med-button').addEventListener('click', handleAddMed);
+document.getElementById('clear-med-list').addEventListener('click', clearMedList);
